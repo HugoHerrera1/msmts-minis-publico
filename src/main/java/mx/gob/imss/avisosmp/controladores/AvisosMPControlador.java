cb987460733f03dc713f6cf54c212659b08c7c1a
HugoHerrera1 <105305874+HugoHerrera1@users.noreply.github.com>
2022-07-15 10:24:10 -0500

    cambio en nombre del requestmapping

--- src/main/java/mx/gob/imss/avisosmp/controladores/AvisosMPControlador.java
+++ src/main/java/mx/gob/imss/avisosmp/controladores/AvisosMPControlador.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/msmts-avisos-mp/api")
+@RequestMapping("/msmts-minis-publico/api")
 public class AvisosMPControlador {
   
 	@Autowired
