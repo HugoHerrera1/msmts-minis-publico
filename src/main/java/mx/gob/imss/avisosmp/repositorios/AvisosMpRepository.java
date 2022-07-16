ee81dc5e46a7e92e15ac87226d43d7a7218e0190
Eduardo Orlando <eduardo.flores@people-media.com.mx>
2022-07-16 14:26:42 -0500

    cambios nombre tabla a Mayuscula

--- src/main/java/mx/gob/imss/avisosmp/repositorios/AvisosMpRepository.java
+++ src/main/java/mx/gob/imss/avisosmp/repositorios/AvisosMpRepository.java
@@ -11,6 +11,6 @@ public interface AvisosMpRepository extends JpaRepository<MtstAvisosMp,Long> {
     @Query(value = "select * from MTST_AVISOS_MP mam where mam.FEC_ELABORACION between ?1 and ?2 and mam.IND_ACTIVO = 1 ",nativeQuery = true)
     List<MtstAvisosMp> findAvisosMp(String fechaInicial, String fechaFinal);
 
-    @Query(value = "select * from mtst_avisos_mp mam where mam.ID_AVISO_MP = ?1 and mam.IND_ACTIVO =1",nativeQuery = true)
+    @Query(value = "select * from MTST_AVISOS_MP mam where mam.ID_AVISO_MP = ?1 and mam.IND_ACTIVO =1",nativeQuery = true)
     MtstAvisosMp findAvisosMpById(Integer idAviso);
 }
