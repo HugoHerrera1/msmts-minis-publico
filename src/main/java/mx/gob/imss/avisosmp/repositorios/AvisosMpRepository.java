9be16e30f49385bd8e66ccb73da12fb712924b93
itzi <wuisi_99@hotmail.com>
2022-07-20 17:45:15 -0500

    ad cache

--- src/main/java/mx/gob/imss/avisosmp/repositorios/AvisosMpRepository.java
+++ src/main/java/mx/gob/imss/avisosmp/repositorios/AvisosMpRepository.java
@@ -1,6 +1,8 @@
 package mx.gob.imss.avisosmp.repositorios;
 
 import mx.gob.imss.avisosmp.modelo.MtstAvisosMp;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,9 +10,13 @@ import java.util.List;
 
 
 public interface AvisosMpRepository extends JpaRepository<MtstAvisosMp,Long> {
-    @Query(value = "select * from MTST_AVISOS_MP mam where mam.FEC_ELABORACION between ?1 and ?2 and mam.IND_ACTIVO = 1 ",nativeQuery = true)
+    
+	@Cacheable("avisosMpfindAvisosMp")
+	@Query(value = "select * from MTST_AVISOS_MP mam where mam.FEC_ELABORACION between ?1 and ?2 and mam.IND_ACTIVO = 1 ",nativeQuery = true)
     List<MtstAvisosMp> findAvisosMp(String fechaInicial, String fechaFinal);
 
+	
+	@Cacheable("avisosMpFindAvisosMpById")
     @Query(value = "select * from MTST_AVISOS_MP mam where mam.ID_AVISO_MP = ?1 and mam.IND_ACTIVO =1",nativeQuery = true)
     MtstAvisosMp findAvisosMpById(Integer idAviso);
 }
